b2f5822ea65630f6eb8a0d10234df4806d7c67bf
Pranav Kosuri <pranavnaidukos@gmail.com>
2026-05-22 16:06:09 +0530

    added the enemy feature, haze feature still left

--- AsciiAdventure.c
+++ AsciiAdventure.c
@@ -1,9 +1,10 @@
 /* started to learn memory management using c and low level Threads .*/
-// add an enemy using threads
+// add an enemy using threads, implement the haze feature 
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<time.h>
+#include<pthread.h>
 
 #define ROWS 10
 #define COLS 10
@@ -18,9 +19,17 @@ typedef struct GOAL {
     int j;
 }goal;
 
+typedef struct ENEMY {
+    int i;
+    int j;
+}enemy;
+
 int found(int pi, int pj, int gi, int gj) {
-    if(gi == pi && gj == pj) return 1;
-    else return 0;
+    return (gi == pi && gj == pj) ? 1 : 0;
+}
+
+int caught(int pi, int pj, int ei, int ej) {
+    return (ei == pi && ej == pj) ? 1 : 0;
 }
 
 char readDirection() {
@@ -38,7 +47,7 @@ typedef struct Cell {
 } cell;
 cell** cells;
 
-void printMaze(cell** cells, int pi, int pj, int gi, int gj) {
+void printMaze(cell** cells, int pi, int pj, int gi, int gj, int ei, int ej) {
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLS; j++) {
             if(cells[i][j].top && i == 0) printf(" __");
@@ -50,7 +59,8 @@ void printMaze(cell** cells, int pi, int pj, int gi, int gj) {
             if(cells[i][j].left &&  j==0) printf("|");
             else if(j == 0) printf(" ");
             if(pi == i && pj == j) printf("@ ");
-            else if(gi == i && gj == j) printf("G ");
+            else if(gi == i && gj == j) printf("$ ");
+            else if(ei == i && ej == j) printf("& ");
             else if(cells[i][j].bottom) printf("__");
             else printf("  ");
             if(cells[i][j].right) printf("|");
@@ -113,6 +123,46 @@ void createMaze(cell** cells, int i, int j) {
     }
 }
 
+int pi;
+int pj;
+int gi;
+int gj;
+
+typedef struct Args {
+    int ei;
+    int ej;
+} args;
+
+void* moveEnemy(void* arg) {
+    args* a = (args*)arg;
+    while(!found(pi,pj,gi,gj)) {
+        if(caught(pi,pj,a->ei,a->ej)) {
+            system("cls");
+            sleep(1);
+            printf("You were caught!");
+            exit(0);
+        }
+        sleep(1);
+        int d = rand() % 4;
+        switch(d){
+            case 0:
+                if(a->ei > 0 && !cells[a->ei - 1][a->ej].bottom) a->ei -= 1;
+                break;
+            case 3:
+                 if(a->ei < ROWS - 1 && !cells[a->ei][a->ej].bottom) a->ei += 1;
+                break;
+            case 1:
+                if(a->ej > 0 && !cells[a->ei][a->ej - 1].right) a->ej -= 1;
+                break;
+            case 2:
+                if(a->ej < COLS -1 && !cells[a->ei][a->ej].right) a->ej += 1;
+                break;
+        }
+        system("cls");
+        printMaze(cells,pi,pj,gi,gj,a->ei,a->ej);
+    }
+}
+
 int main() {
     srand(time(0));
     cells = malloc(ROWS * sizeof(cell*) );
@@ -133,35 +183,55 @@ int main() {
     goal* g = malloc(sizeof(goal));
     g->i = 0;
     g->j = 0;
+
     player* p = malloc(sizeof(player));
     p->i = ROWS-1;
     p->j = COLS/2;
+ 
+    enemy* e = malloc(sizeof(enemy));
+    e->i = 0;
+    e->j = COLS - 1;
+
+    pi = p->i;
+    pj = p->j;
+    gi = g->i;
+    gj = g->j;
     createMaze(cells,ROWS-1,COLS/2);
-    
-    while(!found(p->i,p->j,g->i,g->j)) {
-        printMaze(cells,p->i,p->j,g->i,g->j);
+
+    args* a = malloc(sizeof(args));
+    a->ei = e->i;
+    a->ej = e->i;
+
+    pthread_t new_t; 
+    pthread_create(&new_t, NULL, moveEnemy, a);
+
+    while(!found(pi,pj,gi,gj) && !caught(pi,pj,a->ei,a->ej)) {
+        printMaze(cells,pi,pj,gi,gj,a->ei,a->ej);
         printf("\n");
         char d = readDirection();
         switch(d){
             case 'w':
-                if(p->i > 0 && !cells[p->i][p->j].bottom) p->i -= 1;
+                if(pi > 0 && !cells[pi - 1][pj].bottom) pi -= 1;
                 break;
             case 's':
-                if(p->i < ROWS - 1 && cells[p->i][p->j].top) p->i += 1;
+                 if(pi < ROWS - 1 && !cells[pi][pj].bottom) pi += 1;
                 break;
             case 'a':
-                if(p->j > 0 && cells[p->i][p->j].left) p->j -= 1;
+                if(pj > 0 && !cells[pi][pj - 1].right) pj -= 1;
                 break;
             case 'd':
-                if(p->j < COLS -1 && cells[p->i][p->j].right) p->j += 1;
+                if(pj < COLS -1 && !cells[pi][pj].right) pj += 1;
                 break;
         }
         system("cls");
+        if(found(pi,pj,gi,gj)) printf("gaymamba");
     }
-    if(found) {
+    if(found(pi,pj,gi,gj)) {
         system("cls");
-        printf("You Found the Booty!");
-        usleep(10000);
+        sleep(1);
+        printf("\nYou Found the Booty!");
     }
+
+    pthread_join(new_t, NULL);
    return 0;
 }
